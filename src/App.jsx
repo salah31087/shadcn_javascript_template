a3f1ab2c3918b061f1b0d4b89957d08a568f0df1
Belaid Salah eddine <salahbelaid3108@gmail.com>
2024-10-16 17:00:42 +0100

    Add custome colors and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
-import { Button } from './components/ui/button'
-
 
 export default function App() {
   return (
     <div>
-      <Button>hello there</Button>
     </div>
   )
 }
